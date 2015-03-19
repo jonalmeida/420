fbf20bf37689fe15516a6bd301f6930d22e7a381
Jonathan Almeida <git@jonalmeida.com>
2015-03-19 02:11:05 -0400

    Add thread_id, address attributes to ContactItem

--- project/app/src/main/java/com/jonalmeida/project420/ContactItem.java
+++ project/app/src/main/java/com/jonalmeida/project420/ContactItem.java
@@ -4,11 +4,20 @@ public class ContactItem {
     private String name, lastConversationLine;
     private String profileImage; // String?
     private String dominantColor; // To set the color of the conversation window
+    private int thread_id;
+    private String address; // Phone number
 
     public ContactItem(String name, String lastConversationLine, String profileImage) {
         this.name = name;
         this.lastConversationLine = lastConversationLine;
         this.profileImage = profileImage;
+    }
+
+    public ContactItem(String name, String lastConversationLine, String address, int thread_id) {
+        this.name = name;
+        this.lastConversationLine = lastConversationLine;
+        this.address = address;
+        this.thread_id = thread_id;
     }
 
     public ContactItem(String name, String lastConversationLine) {
@@ -38,5 +47,13 @@ public class ContactItem {
 
     public String getLastConversationLine() {
         return lastConversationLine;
+    }
+
+    public int getThread_id() {
+        return thread_id;
+    }
+
+    public String getAddress() {
+        return address;
     }
 }
