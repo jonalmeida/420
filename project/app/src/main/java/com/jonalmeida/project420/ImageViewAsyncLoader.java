2f83eed325e51ce99f61fef2701502b2f0a73c79
Jonathan Almeida <git@jonalmeida.com>
2015-03-20 14:30:06 -0400

    Empty ImageViewAsyncLoader for contact picture

---
+++ project/app/src/main/java/com/jonalmeida/project420/ImageViewAsyncLoader.java
@@ -0,0 +1,10 @@
+package com.jonalmeida.project420;
+
+import android.os.AsyncTask;
+
+public class ImageViewAsyncLoader extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
