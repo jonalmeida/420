9b301295672622899301cde871666a2125a4f1a6
Jonathan Almeida <git@jonalmeida.com>
2015-03-23 15:56:49 -0400

    Added READ_CONTACTS permission and borked code
    - Fetches incorrect profile image for unknown reasons. Although it's
      probably because we're using 'person' instead of 'contacts' as an id,
      but we don't have a contacts id!
    - Fast scrolling looses the View when it's pushed out of memory. (Edge
      case that needs to be fixed soon.)

--- project/app/src/main/java/com/jonalmeida/project420/ImageViewAsyncLoader.java
+++ project/app/src/main/java/com/jonalmeida/project420/ImageViewAsyncLoader.java
@@ -1,12 +1,19 @@
 package com.jonalmeida.project420;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.WeakReference;
 
 public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
@@ -19,11 +26,53 @@ public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
         imageViewReference = new WeakReference<>(imageView);
     }
 
-    private Bitmap getContactImage(String personId) {
+    private Bitmap getContactImage(String address) {
         // Placeholder for actual code to retrieve user's profile image
-        return BitmapFactory.decodeResource(imageViewContext.getResources(),
-                imageViewContext.getResources()
-                .getIdentifier("ic_add", "drawable", imageViewContext.getPackageName()));
+//        return BitmapFactory.decodeResource(imageViewContext.getResources(),
+//                imageViewContext.getResources()
+//                .getIdentifier("ic_add", "drawable", imageViewContext.getPackageName()));
+
+        if (address != null) {
+//            ContentResolver cr = imageViewContext.getContentResolver();
+//            Uri phoneBookUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(address));
+//
+//            Cursor contact = cr.query(phoneBookUri,
+//                    new String[]{ContactsContract.Contacts._ID}, null, null, null);
+//
+//            if (contact.moveToFirst()) {
+//                long userId = contact.getLong(contact.getColumnIndex(ContactsContract.Contacts._ID));
+//                phoneBookUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, userId);
+//
+//            }
+//            else {
+//                return BitmapFactory.decodeResource(imageViewContext.getResources(), android.R.drawable.ic_menu_report_image);
+//            }
+//
+//            if (phoneBookUri != null) {
+//                InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(
+//                        cr, phoneBookUri);
+//                if (input != null) {
+//                    return BitmapFactory.decodeStream(input);
+//                }
+//            }
+
+            Uri uri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, Long.parseLong(address));
+            Log.d(TAG, "Image uri found: " + uri);
+            InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(imageViewContext.getContentResolver(), uri);
+            if (input == null) {
+                Log.d(TAG, "Couldn't retrieve image, returning null.");
+                return null;
+            }
+            Bitmap res = BitmapFactory.decodeStream(input);
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return res;
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -40,17 +89,19 @@ public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
             bitmap = null;
         }
 
-        Log.d(TAG, "Image done loading!");
-
         ImageView imageView = (ImageView) imageViewReference.get();
         if (imageView != null) {
 
             if (bitmap != null) {
                 imageView.setImageBitmap(bitmap);
-            } else {
-                imageView.setImageDrawable(imageView.getContext().getResources()
-                        .getDrawable(R.drawable.default_icon));
             }
+            // Only use this bit for testing. By default, we use the Android face icon.
+            // This bit of code sets the icon to a 'tick' if we can find a profile image.
+
+            //else {
+            //    imageView.setImageDrawable(imageView.getContext().getResources()
+            //            .getDrawable(R.drawable.ic_tick));
+            //}
         }
 
     }
