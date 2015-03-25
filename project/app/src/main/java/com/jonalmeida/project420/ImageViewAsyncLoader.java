0be20f859be9c0deab9083e79d56865cf70585ab
Jonathan Almeida <git@jonalmeida.com>
2015-03-25 18:42:53 -0400

    A shitty way to get the name to show up
    - TODO: Fix later <- LOL

--- project/app/src/main/java/com/jonalmeida/project420/ImageViewAsyncLoader.java
+++ project/app/src/main/java/com/jonalmeida/project420/ImageViewAsyncLoader.java
@@ -10,24 +10,32 @@ import android.os.AsyncTask;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.lang.ref.WeakReference;
 
 public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
     private static final String TAG = ImageViewAsyncLoader.class.toString();
     private final WeakReference imageViewReference;
+    private final WeakReference nameTextViewReference;
     private final Context imageViewContext;
     private static final String[] PHOTO_ID_PROJECTION = new String[] {
-            ContactsContract.Contacts.PHOTO_ID
+            ContactsContract.Contacts.PHOTO_ID,
+            ContactsContract.Contacts.DISPLAY_NAME
     };
+
+    private String nameFromContact = null;
 
     private static final String[] PHOTO_BITMAP_PROJECTION = new String[] {
             ContactsContract.CommonDataKinds.Photo.PHOTO
     };
 
-    public ImageViewAsyncLoader(Context context, ImageView imageView) {
+    public ImageViewAsyncLoader(Context context, ImageView imageView, TextView name) {
         imageViewContext = context;
         imageViewReference = new WeakReference<>(imageView);
+        nameTextViewReference = new WeakReference<>(name);
     }
 
     private Bitmap getContactImage(String address) {
@@ -63,10 +71,16 @@ public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
         }
 
         ImageView imageView = (ImageView) imageViewReference.get();
+        TextView nameReference = (TextView) nameTextViewReference.get();
         if (imageView != null) {
 
             if (bitmap != null) {
                 imageView.setImageBitmap(bitmap);
+            }
+            if (nameReference != null) {
+                if (nameFromContact != null && !nameFromContact.isEmpty()) {
+                    nameReference.setText(nameFromContact);
+                }
             }
             // Only use this bit for testing. By default, we use the Android face icon.
             // This bit of code sets the icon to a 'tick' if we can find a profile image.
@@ -84,6 +98,7 @@ public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
 
         final Uri uri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
 
+
         try( final Cursor cursor = imageViewContext.
                 getContentResolver().query(
                     uri, PHOTO_ID_PROJECTION, null, null,
@@ -91,6 +106,7 @@ public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
             Integer thumbnailId = null;
             if (cursor.moveToFirst()) {
                 thumbnailId = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.PHOTO_ID));
+                nameFromContact = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
             }
             return thumbnailId;
         }
@@ -114,4 +130,10 @@ public class ImageViewAsyncLoader extends AsyncTask<String, Void, Bitmap> {
         }
 
     }
+
+    final String fetchContactName() {
+        // So hacky it hurts!
+        return nameFromContact;
+    }
+
 }
