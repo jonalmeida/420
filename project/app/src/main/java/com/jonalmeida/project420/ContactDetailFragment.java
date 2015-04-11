c8e52b72da52e66ae251ac32a60b0818bee2b3d4
Jonathan Almeida <git@jonalmeida.com>
2015-04-11 18:05:46 -0400

    Added fancy styling with EditText in CardView

--- project/app/src/main/java/com/jonalmeida/project420/ContactDetailFragment.java
+++ project/app/src/main/java/com/jonalmeida/project420/ContactDetailFragment.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import java.util.LinkedList;
 import java.util.List;
