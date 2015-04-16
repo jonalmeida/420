bc9d2797600f8e797dfd14999dff96920c45a00b
Jonathan Almeida <git@jonalmeida.com>
2015-04-16 19:22:16 -0400

    Added Contact picker for handling numbers

--- project/app/src/main/java/com/jonalmeida/project420/ComposeSmsFragment.java
+++ project/app/src/main/java/com/jonalmeida/project420/ComposeSmsFragment.java
@@ -1,13 +1,19 @@
 package com.jonalmeida.project420;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 /**
@@ -19,14 +25,20 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ComposeSmsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "ComposeSmsFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    /**
+     * For Contact picking
+     */
+    private static final int CONTACT_PICKER_RESULT = 1001;
+    private EditText contactSearch;
+
+    /**
+     * For checking if we're in a master-details view
+     */
+    private static final String IS_TWO_PANE = "two_pane";
+
+    private String mIsTwoPane;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,16 +46,15 @@ public class ComposeSmsFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param twoPane Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment ComposeSmsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ComposeSmsFragment newInstance(String param1, String param2) {
+    public static ComposeSmsFragment newInstance(String twoPane) {
         ComposeSmsFragment fragment = new ComposeSmsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(IS_TWO_PANE, twoPane);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +67,7 @@ public class ComposeSmsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mIsTwoPane = getArguments().getString(IS_TWO_PANE);
         }
     }
 
@@ -65,7 +75,10 @@ public class ComposeSmsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compose_sms, container, false);
+        View v = inflater.inflate(R.layout.fragment_compose_sms, container, false);
+        contactSearch = (EditText) v.findViewById(R.id.compose_contact_search_edit_text);
+        contactSearchEditText(v);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,6 +118,60 @@ public class ComposeSmsFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+    private void contactSearchEditText(View v) {
+
+        contactSearch.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    Intent pickContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                    pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                    startActivityForResult(pickContactIntent, CONTACT_PICKER_RESULT);
+                } else {
+                    // use spinner
+                }
+            }
+        });
+
+        contactSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.d(TAG, "Just typed: " + editable.toString());
+                // TODO: Interact with spinner here
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String phoneNo = null;
+        Uri uri = data.getData();
+        Cursor cursor = getActivity().getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+        phoneNo = cursor.getString(phoneIndex);
+        Log.d(TAG, "We got this number: " + phoneNo);
+
+        if (phoneNo != null) {
+            // TODO: Check if the number exists in thread
+            // If true, open the new activity/fragment with that number
+
+            // Else, new conversation with number in it.
+            contactSearch.append(phoneNo);
+        }
+
+        cursor.close();
     }
 
 }
